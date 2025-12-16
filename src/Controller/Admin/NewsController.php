7edee0b7acc93e4d1ad824084a57391af719b529
Jonas Poli <jonaspoli@gmail.com>
2025-12-16 10:09:20 -0300

    Ajustes nas noticias

--- src/Controller/Admin/NewsController.php
+++ src/Controller/Admin/NewsController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/news')]
 #[IsGranted('ROLE_ADMIN')]
@@ -25,13 +26,16 @@ class NewsController extends AbstractController
     }
 
     #[Route('/new', name: 'admin_news_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $news = new News();
         $form = $this->createForm(NewsType::class, $news);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            if (empty($news->getSlug())) {
+                $news->setSlug($this->generateSlug($news->getTitle(), $slugger));
+            }
             $entityManager->persist($news);
             $entityManager->flush();
 
@@ -47,12 +51,15 @@ class NewsController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'admin_news_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, News $news, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, News $news, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(NewsType::class, $news);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            if (empty($news->getSlug())) {
+                $news->setSlug($this->generateSlug($news->getTitle(), $slugger));
+            }
             $entityManager->flush();
 
             $this->addFlash('success', 'Notícia atualizada com sucesso!');
@@ -76,5 +83,123 @@ class NewsController extends AbstractController
         }
 
         return $this->redirectToRoute('admin_news_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    private function generateSlug(string $title, SluggerInterface $slugger): string
+    {
+        $stopWords = [
+            'o',
+            'a',
+            'os',
+            'as',
+            'um',
+            'uns',
+            'uma',
+            'umas',
+            'de',
+            'do',
+            'da',
+            'dos',
+            'das',
+            'em',
+            'no',
+            'na',
+            'nos',
+            'nas',
+            'por',
+            'pelo',
+            'pela',
+            'pelos',
+            'pelas',
+            'para',
+            'pra',
+            'pro',
+            'com',
+            'sem',
+            'sob',
+            'sobre',
+            'entre',
+            'ante',
+            'ate',
+            'contra',
+            'desde',
+            'e',
+            'ou',
+            'mas',
+            'nem',
+            'que',
+            'se',
+            'como',
+            'pois',
+            'porque',
+            'eu',
+            'voce',
+            'ele',
+            'ela',
+            'nos',
+            'eles',
+            'elas',
+            'meu',
+            'minha',
+            'teu',
+            'tua',
+            'seu',
+            'sua',
+            'nosso',
+            'nossa',
+            'esse',
+            'essa',
+            'isso',
+            'este',
+            'esta',
+            'isto',
+            'aquele',
+            'aquela',
+            'aquilo',
+            'qual',
+            'quais',
+            'quem',
+            'onde',
+            'ser',
+            'e',
+            'eh',
+            'foi',
+            'fom',
+            'sao',
+            'era',
+            'eram',
+            'estar',
+            'esta',
+            'estao',
+            'estava',
+            'estavam',
+            'ter',
+            'tem',
+            'tinha',
+            'tinham',
+            'teve',
+            'muito',
+            'mais',
+            'menos',
+            'ja',
+            'agora',
+            'tambe',
+            'so',
+            'talvez'
+        ];
+
+        // Convert to lowercase and create slug
+        $slug = $slugger->slug(strtolower($title))->toString();
+
+        // Split by dashes (slugger replaces spaces with dashes)
+        $words = explode('-', $slug);
+
+        // Filter out stop words
+        $filteredWords = array_filter($words, function ($word) use ($stopWords) {
+            return !in_array($word, $stopWords) && !empty($word);
+        });
+
+        // Rejoin
+        return implode('-', $filteredWords);
     }
 }
